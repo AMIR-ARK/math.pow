13022f0ec3b15c74aefb53430fe549e983647583
AMIR-ARK <126375003+AMIR-ARK@users.noreply.github.com>
2023-06-25 08:34:56 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a value for x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        double result = CalculateExpression(x);
+
+        Console.WriteLine("The result of the expression is: {0}", result);
+
+        Console.ReadKey();
+    }
+
+    static double CalculateExpression(double x)
+    {
+        double pow = Math.Pow(x, 2);
+        double result = pow + (2 * x) - 4;
+
+        return result;
+    }
+}
